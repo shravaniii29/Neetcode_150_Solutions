445119a579ec2c20f9d77782ab82a05988bbff93
shravaniii29 <sbhole295@gmail.com>
2026-07-17 00:16:00 +0530

    Two Pointers - Medium and Hard

--- 1_Contains_Duplicates.py
+++ 1_Contains_Duplicates.py
@@ -1,6 +1,7 @@
+#ARRAY PROBLEM - Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
+        nums.sort() # very important to sort the array first, so that duplicates are adjacent to each other
         i = 0
         for j in range(1, len(nums)):
             if nums[j] == nums[i]:
